74b6569997d74b69f94b4f036ecda59921f392f0
Valenciaal <valeaal@uma.es>
2023-11-26 19:50:05 +0100

    Expedientes

--- Ejercicio 1/Andamiaje/src/Expediente.java
+++ Ejercicio 1/Andamiaje/src/Expediente.java
@@ -1,9 +1,50 @@
+import java.util.Enumeration;
+import java.util.List;
+
 public class Expediente {
-    private Paciente paciente;
 
-    public Paciente getPaciente() {
-        return this.paciente;
-    }
+	// Atributos referentes a otras clases
+	private List<Acceso> accesos;
+	private Paciente paciente;
 
+	public Expediente(Paciente paciente) throws AssertionError {
 
+		// Acceso no se inicializa porque puede ser cero
+		this.paciente = paciente;
+
+		// [Restricción] El paciente no puede ser ser nulo
+		assert (paciente != null);
+
+		// Las clases con las que tiene relacion tambien tienen que actualizarse
+		paciente.addExpediente(this);
+	}
+
+	public Enumeration<Acceso> getAccesos(){
+		return java.util.Collections.enumeration(accesos);
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) throws AssertionError {
+		// [Restricción] El paciente no puede ser ser nulo
+		assert (paciente != null);
+
+		this.paciente = paciente;
+	}
+
+	protected void addAcceso(Acceso a) throws AssertionError {
+		// [Restricción] El acceso no puede ser ser nulo
+		assert (a != null);
+		
+		this.accesos.add(a);
+	}
+
+	protected void rmvAcceso(Acceso a) throws AssertionError {
+		// [Restricción] El acceso no puede ser ser nulo
+		assert (a != null);
+		
+		this.accesos.remove(a);
+	}
 }
